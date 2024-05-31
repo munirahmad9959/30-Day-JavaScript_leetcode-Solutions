a25bf00c1ad85942107dc298956bbb1b637ad37d
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-05-31 23:35:23 +0500

    Sleep.js added

---
+++ Sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, millis);
+    })
+
+}
+
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) 
