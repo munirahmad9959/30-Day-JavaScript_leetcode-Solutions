367a969852826f779b5359efe8bf0ef0d6c23510
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-06-13 00:55:40 +0500

    Sort By.js added

--- Sort By.js
+++ Sort By.js
@@ -3,14 +3,15 @@
  * @param {Function} fn
  * @return {Array}
  */
-function sortByFunction(arr, fn) {
+var sortBy = function (arr, fn) {
     let mappedArr = arr.map((element) => [fn(element), element]);
 
-    mappedArr.sort((a, b) => a[0] - b[0]);
+    mappedArr.sort((a,b) => a[0] - b[0])
 
     return mappedArr.map((element) => element[1]);
-}
+};
 
-console.log(sortByFunction([5, 4, 1, 2, 3], (x) => x)); // Output: [1, 2, 3, 4, 5]
-console.log(sortByFunction([{"x": 1}, {"x": 0}, {"x": -1}], (d) => d.x)); // Output: [{"x": -1}, {"x": 0}, {"x": 1}]
-console.log(sortByFunction([[3, 4], [5, 2], [10, 1]], (x) => x[1])); // Output: [[10, 1], [5, 2], [3, 4]]
+
+console.log(sortBy([5, 4, 1, 2, 3], (x) => x)); // Output: [1, 2, 3, 4, 5]
+console.log(sortBy([{"x": 1}, {"x": 0}, {"x": -1}], (d) => d.x)); // Output: [{"x": -1}, {"x": 0}, {"x": 1}]
+console.log(sortBy([[3, 4], [5, 2], [10, 1]], (x) => x[1])); // Output: [[10, 1], [5, 2], [3, 4]]
