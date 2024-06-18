88f49faf14a64e714923a10f1db7fd1bd5a9252b
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-06-19 00:24:24 +0500

    .\Flatten Deepy Nested Arrays.js

--- Flatten Deepy Nested Arrays.js
+++ Flatten Deepy Nested Arrays.js
@@ -1,22 +1,28 @@
 /**
- * @param {Array} arr
- * @param {number} depth
+ * @param {Array} array 
+ * @param {number} depth 
  * @return {Array}
  */
-var flat = function (arr, n) {
+var flat = function (array, n) {
+    if (n === 0) return array;
     
-    let result = []
+    const result = [];
+    
+    const flatten = (elements, currentDepth = 0) => {
+        for (let element of elements) {
+            if (Array.isArray(element) && currentDepth < n) {
+                flatten(element, currentDepth + 1);
+            } else {
+                result.push(element);
+            }
+        }
+    };
 
-    if(n === 0){
-        return arr;
-    } else {
-        arr.forEach((element) => {
-            result = result.concat(Array.isArray(element)? flat(element, n - 1):[element]);
-        })
-    }
-    return result;
+    flatten(array);
     
+    return result;
 };
+
 
 
 const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], n = 2;
