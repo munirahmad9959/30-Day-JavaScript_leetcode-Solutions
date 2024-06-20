7aa9637d0f333cf5bbc0317e25a0538a69d44230
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-06-20 23:45:48 +0500

    Compact Object.js added

--- Compact Object.js
+++ Compact Object.js
@@ -2,24 +2,37 @@
  * @param {Object|Array} obj
  * @return {Object|Array}
  */
-var compactObject = function(obj) {
-    
-    if(typeof obj === 'object'){
-        const compactObject = {}
-        for(const keys in obj){
-            if(!obj[keys] && obj[keys] !== false){
-                delete obj[keys]
+var compactObject = function (obj) {
+
+    if (Array.isArray(obj)) {
+        return obj.filter(element => {
+            if (element === null) return false;
+            if (Array.isArray(element) || typeof element === 'object') {
+                return compactObject(element).length > 0
+            }
+            return Boolean(element);
+        });
+    } else if (typeof obj === 'object' && obj !== null) {
+        const compactedObject = {};
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                const value = obj[key];
+                if (value === null) continue;
+                if (Array.isArray(value) || typeof value === 'object') {
+                    const compactedValue = compactObject(value);
+                    if (compactedValue.length > 0) {
+                        compactedObject[key] = compactedValue;
+                    }
+                } else if (Boolean(value)) {
+                    compactedObject[key] = value;
+                }
             }
         }
-        compactObject = {...obj}
-        return compactObject
+        return compactedObject;
     } else {
-        const compactObject = []
-        obj.forEach((elements) => {
-            if(elements){
-                compactObject.push(elements);
-            }
-        })
-        return compactObject;
+        return obj;
     }
 };
+
+const obj = [null, 0, 5, [0], [false, 16]];
+console.log(compactObject(obj));
