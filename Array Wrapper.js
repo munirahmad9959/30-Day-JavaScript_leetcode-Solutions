249ad4604927171bf2e7721055d429eeaed09b43
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-06-23 00:03:37 +0500

    Array Wrapper.js added

--- Array Wrapper.js
+++ Array Wrapper.js
@@ -10,23 +10,23 @@ var ArrayWrapper = function(nums) {
  * @return {number}
  */
 ArrayWrapper.prototype.valueOf = function() {
-    return this.nums.reduce((accum, nums) => {
-        accum += nums;
-    }, 0)
-}
+    return this.nums.reduce((accum, num) => {
+        return accum + num; 
+    }, 0);
+};
 
 /**
  * @return {string}
  */
 ArrayWrapper.prototype.toString = function() {
-    return this.nums.join();
+    return `[${this.nums.join(', ')}]`
 }
 
 
 
 const obj1 = new ArrayWrapper([1,2]);
 const obj2 = new ArrayWrapper([3,4]);
-console.log(obj1 + obj2) // 10
-// String(obj1); // "[1,2]"
-// String(obj2); // "[3,4]"
+console.log(obj1 + obj2); // 10
+console.log(String(obj1)); // "[1,2]"
+console.log(String(obj2)); // "[3,4]"
 
